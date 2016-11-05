c2368e888d4ae9dc9a0a579351082549e7620f8d
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-11-05 14:15:43 -0200

    Logo and Login screens

--- app/src/main/java/com/hammerox/bluehack/MainActivity.java
+++ app/src/main/java/com/hammerox/bluehack/MainActivity.java
@@ -1,7 +1,12 @@
 package com.hammerox.bluehack;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    @OnClick(R.id.activity_main)
+    public void clickOnFullScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
