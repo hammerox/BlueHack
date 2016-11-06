68728e53cebf55c2d559fb10e637a74210934943
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-11-06 11:23:59 -0200

    InterestActivity finished

--- app/src/main/java/com/hammerox/bluehack/InterestActivity.java
+++ app/src/main/java/com/hammerox/bluehack/InterestActivity.java
@@ -1,16 +1,32 @@
 package com.hammerox.bluehack;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
+import butterknife.BindColor;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static android.view.View.GONE;
+
 public class InterestActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_interest) RelativeLayout layout;
+    @BindView(R.id.interest_scroll) ScrollView scrollView;
+    @BindView(R.id.interest_button) Button interestButton;
+    @BindColor(R.color.colorPrimary) int colorPrimary;
+
+    private int interestCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +40,38 @@ public class InterestActivity extends AppCompatActivity {
 
     @OnClick({ R.id.interest_image1, R.id.interest_image2, R.id.interest_image3})
     public void onImageClick(ImageView imageView){
-        imageView.getDrawable().setColorFilter(
-                new PorterDuffColorFilter(Color.GREEN, PorterDuff.Mode.MULTIPLY));
+
+        String idString = getResources().getResourceEntryName(imageView.getId());
+        String number = idString.split("image")[1];
+        String newIdName = "interest_okay" + number;
+
+        int resourceId = this.getResources().getIdentifier(newIdName, "id", this.getPackageName());
+
+        findViewById(resourceId).setVisibility(View.VISIBLE);
+        interestCount++;
+
+        if (interestCount == 1) {
+            interestButton.setVisibility(View.VISIBLE);
+
+            interestButton.getBackground().setColorFilter(
+            new PorterDuffColorFilter(colorPrimary, PorterDuff.Mode.MULTIPLY));
+        }
+    }
+
+    @OnClick({ R.id.interest_okay1, R.id.interest_okay2, R.id.interest_okay3})
+    public void onCheckClick(ImageView imageView) {
+        imageView.setVisibility(GONE);
+        interestCount--;
+
+        if(interestCount == 0) {
+            interestButton.setVisibility(GONE);
+        }
+    }
+
+    @OnClick(R.id.interest_button)
+    public void onOkayClick() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
